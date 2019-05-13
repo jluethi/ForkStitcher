011682acd8753fbc34233ab2cc1123961648db6b
Joel Lüthi <joel.luethi@uzh.ch>
2019-05-13 18:33:27 +0200

    Add a threshold to the find_annotation_tile function to ensure that the nearest tile actually contains the annotation and various cleanup

--- sites_of_interest_parser.py
+++ sites_of_interest_parser.py
@@ -6,14 +6,16 @@ import numpy as np
 import math
 from pathlib import Path
 
+
 class Xml_MAPS_Parser():
 
-    def __init__(self, project_folder, name_of_highmag_layer = 'highmag', position_to_use = 0, stitch_radius = 1):
+    def __init__(self, project_folder, name_of_highmag_layer='highmag', position_to_use=0, stitch_radius=1):
         self.project_folder_path = Path(project_folder)
         self.name_of_highmag_layer = name_of_highmag_layer
 
-        # In the samples I had, MAPS didn't show any aligned positions. Therefore, unaligned positions is what we want to
-        # extract. If positions are moved somehow in MAPS (I assume through stitching), calculatedPositions may be revelant
+        # In the samples I had, MAPS didn't show any aligned positions. Therefore, unaligned positions is what
+        # we want to extract. If positions are moved somehow in MAPS (I assume through stitching),
+        # calculatedPositions may be revelant
         potential_positions_to_extract = ['UnalignedPosition', 'CalculatedPosition']
         self.position_to_extract = potential_positions_to_extract[position_to_use]
         xml_file_name = 'MapsProject.xml'
@@ -27,7 +29,6 @@ class Xml_MAPS_Parser():
         self.tile_center_stage_positions = []
         self.annotation_tiles = {}
         self.stitch_radius = stitch_radius
-
 
     def load_xml(self, xml_file_path):
         # Load the MAPS XML File
@@ -91,22 +92,27 @@ class Xml_MAPS_Parser():
                                             for layer_content in square:
                                                 if layer_content.tag.endswith('totalHfw'):
                                                     # noinspection PyUnboundLocalVariable
-                                                    self.squares[metadata_location]['totalHfw'] = float(list(layer_content.attrib.values())[1])
+                                                    self.squares[metadata_location]['totalHfw'] = float(
+                                                        list(layer_content.attrib.values())[1])
 
                                                 if layer_content.tag.endswith('tileHfw'):
                                                     # noinspection PyUnboundLocalVariable
-                                                    self.squares[metadata_location]['tileHfw'] = float(list(layer_content.attrib.values())[1])
+                                                    self.squares[metadata_location]['tileHfw'] = float(
+                                                        list(layer_content.attrib.values())[1])
 
                                                 if layer_content.tag.endswith('overlapHorizontal'):
                                                     # noinspection PyUnboundLocalVariable
-                                                    self.squares[metadata_location]['overlapHorizontal'] = float(layer_content[0].text)/100.
+                                                    self.squares[metadata_location]['overlapHorizontal'] = float(
+                                                        layer_content[0].text) / 100.
                                                 if layer_content.tag.endswith('overlapVertical'):
                                                     # noinspection PyUnboundLocalVariable
-                                                    self.squares[metadata_location]['overlapVertical'] = float(layer_content[0].text)/100.
+                                                    self.squares[metadata_location]['overlapVertical'] = float(
+                                                        layer_content[0].text) / 100.
 
                                                 if layer_content.tag.endswith('rotation'):
                                                     # noinspection PyUnboundLocalVariable
-                                                    self.squares[metadata_location]['rotation'] = float(list(layer_content.attrib.values())[1])
+                                                    self.squares[metadata_location]['rotation'] = float(
+                                                        list(layer_content.attrib.values())[1])
 
                                                 if layer_content.tag.endswith('rows'):
                                                     # noinspection PyUnboundLocalVariable
@@ -120,23 +126,28 @@ class Xml_MAPS_Parser():
                                                     for scanres_info in layer_content:
                                                         if scanres_info.tag.endswith('height'):
                                                             # noinspection PyUnboundLocalVariable
-                                                            self.squares[metadata_location]['img_height'] = int(scanres_info.text)
+                                                            self.squares[metadata_location]['img_height'] = int(
+                                                                scanres_info.text)
                                                         if scanres_info.tag.endswith('width'):
                                                             # noinspection PyUnboundLocalVariable
-                                                            self.squares[metadata_location]['img_width'] = int(scanres_info.text)
+                                                            self.squares[metadata_location]['img_width'] = int(
+                                                                scanres_info.text)
 
                                                 if layer_content.tag.endswith('pixelSize'):
-                                                        # noinspection PyUnboundLocalVariable
-                                                        self.squares[metadata_location]['pixel_size'] = float(layer_content.attrib['Value'])
+                                                    # noinspection PyUnboundLocalVariable
+                                                    self.squares[metadata_location]['pixel_size'] = float(
+                                                        layer_content.attrib['Value'])
 
                                                 if layer_content.tag.endswith('StagePosition'):
                                                     for positon_info in layer_content:
                                                         if positon_info.tag == '{http://schemas.datacontract.org/2004/07/Fei.Applications.SAL}x':
                                                             # noinspection PyUnboundLocalVariable
-                                                            self.squares[metadata_location]['StagePosition_center_x'] = float(positon_info.text)
+                                                            self.squares[metadata_location][
+                                                                'StagePosition_center_x'] = float(positon_info.text)
                                                         elif positon_info.tag == '{http://schemas.datacontract.org/2004/07/Fei.Applications.SAL}y':
                                                             # noinspection PyUnboundLocalVariable
-                                                            self.squares[metadata_location]['StagePosition_center_y'] = float(positon_info.text)
+                                                            self.squares[metadata_location][
+                                                                'StagePosition_center_y'] = float(positon_info.text)
                                         except NameError:
                                             print("Can't find the metaDataLocation in the MAPS XML File")
                                             sys.exit(-1)
@@ -174,7 +185,8 @@ class Xml_MAPS_Parser():
         metadata_filename = 'StitchingData.xml'
         for metadata_location in self.squares:
             tile_image_folder_path = ''
-            metadata_path = self.project_folder_path.joinpath(self.convert_windows_pathstring_to_path_object(metadata_location)) / metadata_filename
+            metadata_path = self.project_folder_path.joinpath(
+                self.convert_windows_pathstring_to_path_object(metadata_location)) / metadata_filename
             metadata_root = ET.parse(metadata_path).getroot()
             for metadata_child in metadata_root:
                 if metadata_child.tag.endswith('tileSet'):
@@ -194,24 +206,27 @@ class Xml_MAPS_Parser():
                                                     if value.tag.endswith('ImageFileName'):
                                                         # noinspection PyUnboundLocalVariable
                                                         tile_name = current_square + '_' + value.text
-                                                        self.tiles[tile_name] = {'square' : metadata_location,
-                                                                            'img_path': tile_image_folder_path,
-                                                                            'filename': value.text,
-                                                                            'square_name': current_square}
+                                                        self.tiles[tile_name] = {'square': metadata_location,
+                                                                                 'img_path': tile_image_folder_path,
+                                                                                 'filename': value.text,
+                                                                                 'square_name': current_square}
 
                                                 for value in keyvalue:
                                                     if value.tag.endswith('PositioningDetails'):
                                                         for positioning_detail in value:
-                                                            if positioning_detail.tag.endswith(self.position_to_extract):
+                                                            if positioning_detail.tag.endswith(
+                                                                    self.position_to_extract):
                                                                 for position in positioning_detail:
 
                                                                     if position.tag == '{http://schemas.datacontract.org/2004/07/System.Drawing}x':
                                                                         # noinspection PyUnboundLocalVariable
-                                                                        self.tiles[tile_name]['RelativeTilePosition_x'] = float(
+                                                                        self.tiles[tile_name][
+                                                                            'RelativeTilePosition_x'] = float(
                                                                             position.text)
                                                                     elif position.tag == '{http://schemas.datacontract.org/2004/07/System.Drawing}y':
                                                                         # noinspection PyUnboundLocalVariable
-                                                                        self.tiles[tile_name]['RelativeTilePosition_y'] = float(
+                                                                        self.tiles[tile_name][
+                                                                            'RelativeTilePosition_y'] = float(
                                                                             position.text)
 
     def calculate_absolute_tile_coordinates(self):
@@ -222,12 +237,13 @@ class Xml_MAPS_Parser():
         current_square = self.squares[current_square_key]
 
         # Unsure if it's height/width or width/height because they are always the same on Talos
-        current_square['tileVfw'] = current_square['img_height'] / current_square['img_width'] * current_square['tileHfw']
+        current_square['tileVfw'] = current_square['img_height'] / current_square['img_width'] * current_square[
+            'tileHfw']
 
         horizontal_field_width = (current_square['columns'] - 1) * current_square['tileHfw'] * (
-                    1 - current_square['overlapHorizontal']) + current_square['tileHfw']
+                1 - current_square['overlapHorizontal']) + current_square['tileHfw']
         vertical_field_width = (current_square['rows'] - 1) * current_square['tileVfw'] * (
-                    1 - current_square['overlapHorizontal']) + current_square['tileVfw']
+                1 - current_square['overlapHorizontal']) + current_square['tileVfw']
 
         relative_0_x = current_square['StagePosition_center_x'] - math.sin(
             current_square['rotation'] / 180 * math.pi) * vertical_field_width / 2 + math.cos(
@@ -243,16 +259,20 @@ class Xml_MAPS_Parser():
         for current_tile_name in self.tiles:
             current_tile = self.tiles[current_tile_name]
 
-            relative_x_stepsize = np.array([current_square['pixel_size'] * math.cos(current_square['rotation'] / 180 * math.pi),
+            relative_x_stepsize = np.array([current_square['pixel_size'] * math.cos(current_square['rotation']
+                                                                                    / 180 * math.pi),
                                             current_square['pixel_size'] * math.sin(
                                                 current_square['rotation'] / 180 * math.pi)])
-            relative_y_stepsize = np.array([current_square['pixel_size'] * math.sin(current_square['rotation'] / 180 * math.pi),
+            relative_y_stepsize = np.array([current_square['pixel_size'] * math.sin(current_square['rotation']
+                                                                                    / 180 * math.pi),
                                             current_square['pixel_size'] * math.cos(
                                                 current_square['rotation'] / 180 * math.pi)])
 
-            # absolute_tile_pos is the position of the corner of the tile in absolute Stage Position coordinates, the tile_center_stage_position are the Stage Position coordinates of the tile
-            absolute_tile_pos = relative_0 + current_tile['RelativeTilePosition_x'] * relative_x_stepsize + current_tile[
-                'RelativeTilePosition_y'] * relative_y_stepsize
+            # absolute_tile_pos is the position of the corner of the tile in absolute Stage Position coordinates,
+            # the tile_center_stage_position are the Stage Position coordinates of the tile
+            absolute_tile_pos = relative_0 + current_tile['RelativeTilePosition_x'] * relative_x_stepsize + \
+                                current_tile[
+                                    'RelativeTilePosition_y'] * relative_y_stepsize
             tile_center_stage_position = absolute_tile_pos + current_square['img_width'] / 2 * relative_x_stepsize + \
                                          current_square['img_height'] / 2 * relative_y_stepsize
 
@@ -260,20 +280,35 @@ class Xml_MAPS_Parser():
             self.tile_names.append([current_square['square_name'], current_tile_name])
 
     def find_annotation_tile(self):
-        # Give annotation_name, square metadata key, square_name, tile_name, stage coordinates & pixel position in the image
+        # Give annotation_name, square metadata key, square_name, tile_name,
+        # stage coordinates & pixel position in the image
         # TODO: Save the other relevant output information besides the tile_name
+
+        # If the min distance is smaller than the diagonal distance to the edge of the image from its center,
+        # we have found the closest tile and the annotation is within that tile.
+        # If the distance is larger than the diagonal distance to the edge, the annotation is not inside of any tile
+        # and thus shouldn't be exported
+        # This calculation contains the assumption that the image dimensions & pixel size of the first square
+        # is applicable for all squares
+        first_square = self.squares[list(self.squares.keys())[0]]
+        distance_threshold = np.square(first_square['img_height'] / 2 * first_square['pixel_size']) + \
+                             np.square(first_square['img_width'] / 2 * first_square['pixel_size'])
+
         for annotation_name in self.annotations:
-            a_coordinates = np.array([self.annotations[annotation_name]['StagePosition_x'], self.annotations[annotation_name]['StagePosition_y']])
+            a_coordinates = np.array([self.annotations[annotation_name]['StagePosition_x'],
+                                      self.annotations[annotation_name]['StagePosition_y']])
             distance_map = np.square(np.array(self.tile_center_stage_positions) - a_coordinates)
             quandratic_distance = distance_map[:, 0] + distance_map[:, 1]
             tile_index = np.argmin(quandratic_distance)
-            # TODO: Deal with the possibility of equi-distant tiles
+            # noinspection PyTypeChecker
             current_tile = self.tiles[self.tile_names[tile_index][1]]
-            self.annotation_tiles[annotation_name] = current_tile
+
+            if quandratic_distance[tile_index] < distance_threshold:
+                self.annotation_tiles[annotation_name] = current_tile
+            else:
+                print('WARNING! Annotation {} is not within any of the tiles and will be ignored'.format(annotation_name))
 
             # TODO: Calculate the position of the fork within the image and return it as well
-
-            # TODO: Include a threshold to discard forks if they are outside of any tiles
 
     def determine_surrounding_sites(self):
         # Take in the center tile and determine the names & existence of stitch_radius tiles around it.
@@ -293,7 +328,8 @@ class Xml_MAPS_Parser():
                     # self.annotation_tiles[annotation_name]['surrounding_tile_names'].append(new_filename)
 
                     # Check whether those files exist
-                    img_path = self.convert_img_path_to_local_path(self.annotation_tiles[annotation_name]['img_path']) / new_filename
+                    img_path = self.convert_img_path_to_local_path(
+                        self.annotation_tiles[annotation_name]['img_path']) / new_filename
 
                     if img_path.is_file():
                         self.annotation_tiles[annotation_name]['surrounding_tile_exists'].append(True)
@@ -301,15 +337,14 @@ class Xml_MAPS_Parser():
                         self.annotation_tiles[annotation_name]['surrounding_tile_exists'].append(False)
 
 
-
-
-
 def main():
     pass
-    # project_folder_path = '/Volumes/staff/zmbstaff/7831/Raw_Data/Group Lopes/Sebastian/Projects/8330_siNeg_CPT_3rd/'
-    # highmag_layer = 'highmag'
-    # parser = Xml_MAPS_Parser(project_folder= project_folder_path, position_to_use=0, name_of_highmag_layer=highmag_layer)
-    #
+    project_folder_path = '/Volumes/staff/zmbstaff/7831/Raw_Data/Group Lopes/Sebastian/Projects/8330_siNeg_CPT_3rd/'
+    highmag_layer = 'highmag'
+    parser = Xml_MAPS_Parser(project_folder=project_folder_path, position_to_use=0,
+                             name_of_highmag_layer=highmag_layer, stitch_radius=1)
+
+    parser.parse_xml()
     # parser.extract_square_metadata()
     # parser.extract_annotation_locations()
     # parser.get_relative_tile_locations()
@@ -325,20 +360,10 @@ def main():
     # print(tile_names)
 
 
-
 if __name__ == "__main__":
     main()
-
-
-
-
-
 
 # Change to using findall() with full tags instead of matching end of tags
 # for type_tag in root.findall('bar/type'):
 #     value = type_tag.get('foobar')
 #     print(value)
-
-
-
-
--- stitch_MAPS_annotations.py
+++ stitch_MAPS_annotations.py
@@ -8,20 +8,20 @@ from pathlib import Path
 # Stitching plugin only runs when the local Fiji installation is being used. When a current imageJ gateway is installed,
 # it does not find the plugin
 
-def stitch_annotated_tiles(parser, output_path, stitch_radius=1):
+def stitch_annotated_tiles(xml_parser, output_path, stitch_radius=1):
     # This function takes the parser object containing all the information about the MAPS project and its annotations.
     # It the performs stitching
     # TODO: Find a way to call the Fiji stitching without falling back to the local Fiji library
     # ij = imagej.init('sc.fiji:fiji')
     ij = imagej.init('/Applications/Fiji.app')
-    for annotation_name in parser.annotation_tiles:
-        number_existing_neighbor_tiles = sum(parser.annotation_tiles[annotation_name]['surrounding_tile_exists'])
+    for annotation_name in xml_parser.annotation_tiles:
+        number_existing_neighbor_tiles = sum(xml_parser.annotation_tiles[annotation_name]['surrounding_tile_exists'])
         # If the image files for the 8 neighbors and the tile exist, stitch the images
         if number_existing_neighbor_tiles == 9:
             print('Stitching {}'.format(annotation_name))
-            center_filename = parser.annotation_tiles[annotation_name]['filename']
+            center_filename = xml_parser.annotation_tiles[annotation_name]['filename']
 
-            img_path = parser.convert_img_path_to_local_path(parser.annotation_tiles[annotation_name]['img_path'])
+            img_path = xml_parser.convert_img_path_to_local_path(xml_parser.annotation_tiles[annotation_name]['img_path'])
 
             plugin = 'Grid/Collection stitching'
 
@@ -41,13 +41,19 @@ def stitch_annotated_tiles(parser, output_path, stitch_radius=1):
             output_filename = annotation_name + '.tiff'
             shutil.move(img_path / 'img_t1_z1_c1', output_path / output_filename)
 
-
-
         else:
             # TODO: Potentially implement stitching for edge-cases of non 3x3 tiles
             print(
                 'WARNING! Not stitching fork {} at the moment, because it is not surrounded by other tiles'.format(
                     annotation_name))
+
+
+def save_annotation_info_to_csv(xml_parser):
+    # TODO: Saves the annotation_tiles information to a csv file, so that the fork position could be used in
+    # quality control and so that users assign whether an annotation was a fork or a false positive.
+    # Needs to have the information about fork location both in MAPS (global coordinates) and in the stitched image
+    # (img coordinates)
+    pass
 
 
 def main():
@@ -66,7 +72,7 @@ def main():
     parser.parse_xml()
     print(parser.annotation_tiles['fork74'])
 
-    stitch_annotated_tiles(parser=parser, output_path=output_path, stitch_radius=stitch_radius)
+    stitch_annotated_tiles(xml_parser=parser, output_path=output_path, stitch_radius=stitch_radius)
 
 
 if __name__ == "__main__":
