b66796284eed9586f9045fc909fd15cdb735f0bc
Joel Lüthi <j.luethi@core.uzh.ch>
2019-06-20 15:04:05 +0200

    Add icon and simplify pyinstaller paths

--- fork-stitcher/Stitcher.py
+++ fork-stitcher/Stitcher.py
@@ -7,10 +7,10 @@ import logging
 # If everything is run from a frozen .exe package, set the path variables accordingly. See here for details:
 # https://stackoverflow.com/questions/404744/determining-application-path-in-a-python-exe-generated-by-pyinstaller
 if getattr(sys, 'frozen', False):
-    os.environ['JAVA_HOME'] = os.path.join(os.getcwd(), 'share\\jdk8u212-b04')
-    os.environ['PATH'] = os.path.join(os.getcwd(), 'share\\jdk8u212-b04\\jre\\bin') + os.pathsep + \
-                         os.path.join(os.getcwd(), 'share\\jdk8u212-b04\\jre\\bin\\server') + os.pathsep + \
-                         os.path.join(os.getcwd(), 'share\\apache-maven-3.6.1\\bin') + os.pathsep + os.environ['PATH']
+    os.environ['JAVA_HOME'] = os.path.join(os.getcwd(), 'share\\jdk8')
+    os.environ['PATH'] = os.path.join(os.getcwd(), 'share\\jdk8\\jre\\bin') + os.pathsep + \
+                         os.path.join(os.getcwd(), 'share\\jdk8\\jre\\bin\\server') + os.pathsep + \
+                         os.path.join(os.getcwd(), 'share\\maven\\bin') + os.pathsep + os.environ['PATH']
 
 # This import needs to happen after the paths are set correctly
 from gui import Gui
@@ -19,7 +19,7 @@ def main():
     root = tk.Tk()
     root.title('Fork Stitcher')
 
-    root.geometry("+2000+0")
+    root.geometry("+1000+500")
 
     p = Gui(root)
     root.protocol("WM_DELETE_WINDOW", p.shutdown)
