7cd5d99f3e439135ea21ab97e6f3c0ac1a49f972
Joel Lüthi <j.luethi@core.uzh.ch>
2019-06-21 12:34:25 +0200

    Improving gui

--- fork-stitcher/Stitcher.py
+++ fork-stitcher/Stitcher.py
@@ -14,23 +14,29 @@ if getattr(sys, 'frozen', False):
                          os.path.join(os.getcwd(), 'share\\maven\\bin') + os.pathsep + os.environ['PATH']
 
 
+def get_center_position(master):
+    windowWidth = master.winfo_reqwidth()
+    windowHeight = master.winfo_reqheight()
+    positionRight = int(master.winfo_screenwidth() / 2 - windowWidth / 2)
+    positionDown = int(master.winfo_screenheight() / 2 - windowHeight / 2)
+
+    return (positionRight, positionDown)
+
+
 def main():
     root = tk.Tk()
+
+    root.title('Fork Stitcher')
+
     loading_message = tk.Label(root, text='Loading ... Please wait.')
     loading_message.config(height=2, font=(font.Font(), 24, 'bold'))
     loading_message.grid(row=0, column=0, sticky=tk.E, pady=10, padx=10)
     loading_details = tk.Label(root, text='Loading takes ~ 30s on normal runs and a few minutes on the first run')
     loading_details.grid(row=1, column=0, sticky=tk.E, pady=10, padx=10)
+
+    center_positions = get_center_position(root)
+    root.geometry("+{}+{}".format(center_positions[0], center_positions[1]))
     root.update()
-
-    root.title('Fork Stitcher')
-
-    windowWidth = root.winfo_reqwidth()
-    windowHeight = root.winfo_reqheight()
-    positionRight = int(root.winfo_screenwidth() / 2 - windowWidth / 2)
-    positionDown = int(root.winfo_screenheight() / 2 - windowHeight / 2)
-
-    root.geometry("+{}+{}".format(positionRight, positionDown))
 
     # This import takes time and needs to happen after the paths are set correctly and the loading window is running
     from gui import Gui
